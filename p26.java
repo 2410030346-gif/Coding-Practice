b001d1e129d17598d04137d961b9977e0585e1c6
Niranjan <2410030346@klh.edu.in>
2026-07-09 16:00:22 +0530

    numdecodings

---
+++ p26.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    private Map<String, Boolean> memo = new HashMap<>();
+
+    public boolean isScramble(String s1, String s2) {
+        // Base case: identical strings
+        if (s1.equals(s2)) return true;
+
+        // Quick rejection: different lengths or different character sets
+        if (s1.length() != s2.length()) return false;
+        if (!haveSameChars(s1, s2)) return false;
+
+        String key = s1 + "#" + s2;
+        if (memo.containsKey(key)) return memo.get(key);
+
+        int n = s1.length();
+        for (int i = 1; i < n; i++) {
+            // Case 1: No swap
+            if (isScramble(s1.substring(0, i), s2.substring(0, i)) &&
+                isScramble(s1.substring(i), s2.substring(i))) {
+                memo.put(key, true);
+                return true;
+            }
+            // Case 2: Swap
+            if (isScramble(s1.substring(0, i), s2.substring(n - i)) &&
+                isScramble(s1.substring(i), s2.substring(0, n - i))) {
+                memo.put(key, true);
+                return true;
+            }
+        }
+
+        memo.put(key, false);
+        return false;
+    }
+
+    private boolean haveSameChars(String s1, String s2) {
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+}
